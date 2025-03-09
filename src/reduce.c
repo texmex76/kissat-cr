7c9c15fd074176ea2cfb27e30f6b7f15485ea4f1
Armin Biere <armin.biere@gmail.com>
2025-03-09 19:22:41 +0100

    glue 7 keeping

--- src/reduce.c
+++ src/reduce.c
@@ -81,7 +81,7 @@ static bool collect_reducibles (kissat *solver, reducibles *reds,
     if (c->reason)
       continue;
     const unsigned glue = c->glue;
-    if (glue <= 1)
+    if (glue <= 7)
       continue;
 #if 0
     if (glue <= tier1 && used)

e8ef052895741bd1d3fe64e9179eb87088f3f85f
Florian Pollitt <pollittf@informatik.uni-freiburg.de>
2025-03-14 09:23:46 +0100

    keep-glue2+used

--- src/reduce.c
+++ src/reduce.c
@@ -83,7 +83,7 @@ static bool collect_reducibles (kissat *solver, reducibles *reds,
     if (c->reason)
       continue;
     const unsigned glue = c->glue;
-    if (glue <= 3)
+    if (glue <= 2)
       continue;
 #if 0
     if (glue <= tier1 && used)
